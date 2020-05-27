f88cf57c85a82ad1f6dc0766759a4a276adf92c9
Ella Aronov <ellaarono1994@gmail.com>
2020-05-27 22:25:53 +0300

    Refactored code stracture

--- ask-anything-server/models/index.js
+++ ask-anything-server/models/index.js
@@ -1,24 +1,24 @@
-const Sequelize = require('sequelize');
-const UserModel = require('./user');
-const PostModel = require('./post');
+const Sequelize = require("sequelize");
+const UserModel = require("./user");
+const PostModel = require("./post");
 
-const sequelize = new Sequelize('ask-anything','postgres','ellaaronov',{
-    host: 'localhost',
-    dialect: 'postgres', 
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
+const sequelize = new Sequelize("ask-anything", "postgres", "ellaaronov", {
+  host: "localhost",
+  dialect: "postgres",
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 const User = UserModel(sequelize, Sequelize);
 const Post = PostModel(sequelize, Sequelize);
 
 Post.belongsTo(User, {
-  constraints: true, 
-  onDelete: 'CASCADE'
+  constraints: true,
+  onDelete: "CASCADE",
 });
 
 User.hasMany(Post);
@@ -27,4 +27,4 @@ module.exports = {
   User,
   Post,
   sequelize,
-}
+};
